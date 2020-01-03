717e44bf09a21dd92ed1b3aa05cc7012935385ba
Kevinipi <kevin.pose@ipilyon.net>
2020-01-03 11:27:21 +0100

    Insert PagingAndSortingRepository in java class 'artist' and 'Album' for initialize project

--- src/main/java/com/ipiecoles/java/audio/Repository/AlbumRepository.java
+++ src/main/java/com/ipiecoles/java/audio/Repository/AlbumRepository.java
@@ -1,4 +1,7 @@
 package com.ipiecoles.java.audio.Repository;
 
-public interface AlbumRepository {
+import com.ipiecoles.java.audio.model.Album;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AlbumRepository extends PagingAndSortingRepository <Album, Long> {
 }
--- src/main/java/com/ipiecoles/java/audio/Repository/ArtistRepository.java
+++ src/main/java/com/ipiecoles/java/audio/Repository/ArtistRepository.java
@@ -1,4 +1,7 @@
 package com.ipiecoles.java.audio.Repository;
 
-public interface ArtistRepository {
+import com.ipiecoles.java.audio.model.Artist;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ArtistRepository extends PagingAndSortingRepository <Artist, Long> {
 }
