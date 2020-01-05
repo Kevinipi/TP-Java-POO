898198febeda4bf2a21e1934513f468aa14d9508
Kevinipi <kevin.pose@ipilyon.net>
2020-01-05 10:44:07 +0100

    Create variable private and generate constructor and Getter/Setter

--- src/main/java/com/ipiecoles/java/audio/model/Album.java
+++ src/main/java/com/ipiecoles/java/audio/model/Album.java
@@ -7,10 +7,45 @@ import javax.persistence.Table;
 
 @Table(name="Artist")
 public abstract class Album {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    public Album() {
-    }
+    private Long albumid;
+
+    private String title;
+
+    private Long artistid;
+
+    //Constructor
+        public Album(Long albumid, String title, Long artistid) {
+            this.albumid = albumid;
+            this.title = title;
+            this.artistid = artistid;
+        }
+
+        //Getter and Setter
+        public Long getAlbumid() {
+            return albumid;
+        }
+
+        public void setAlbumid(Long albumid) {
+            this.albumid = albumid;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public Long getArtistid() {
+            return artistid;
+        }
+
+        public void setArtistid(Long artistid) {
+            this.artistid = artistid;
+        }
+    //End Constructor
 }
 
--- src/main/java/com/ipiecoles/java/audio/model/Artist.java
+++ src/main/java/com/ipiecoles/java/audio/model/Artist.java
@@ -8,10 +8,33 @@ import javax.persistence.Id;
 @Table(name="Album")
 
 public abstract class Artist {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long artistid;
+
+    private String nom;
 
     //Contructor
-    public Artist() {
-    }
+        public Artist(Long id, String nom) {
+            this.artistid = id;
+            this.nom = nom;
+        }
     //Getter and Setter
 
+        public Long getId() {
+            return artistid;
+        }
+
+        public void setId(Long id) {
+            this.artistid = id;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+    //End Constructor
 }
